626f62be2bcf2eeca8386d8a485ab817ad36df95
president-tuychiyev <president7566@gmail.com>
2024-05-26 18:36:33 +0500

    fix

--- app/Helpers/request.php
+++ app/Helpers/request.php
@@ -1,6 +1,6 @@
 <?php
 
-function request()
+function requestBot()
 {
     is_null(json_decode(file_get_contents('php://input'))) ? $data = $_REQUEST : $data = json_decode(file_get_contents('php://input'));
 
